4b53330019534eba1feac84d58d36e865cc1edd3
Ambroise Mostin <ambroise.mostin@amoobi.com>
2022-03-14 12:14:40 +0100

    add comment

--- src/components/TagActivation/ActivateBasicTag.js
+++ src/components/TagActivation/ActivateBasicTag.js
@@ -3,8 +3,23 @@ import CardBasicTitle from "../Cards/CardBasicTitle";
 import {getRecentTags, postBasicTag} from "../../services/omgServer";
 import TextField from '@material-ui/core/TextField';
 
+/**
+ * This class represents a card with 4 sections :
+ * 1. recent tags will display the 8 most recent tag names
+ * 2. manual will get a new tag name
+ * 3. datetime will get the new event datetime
+ * 4. button will send the tag name and datetime
+ */
 class ActivateBasicTag extends Component {
-
+    /**
+     * recentTags will contain all the tag names
+     * roundedDatetime will contain the rounded datetime to send to the DB.
+     * chosenDatetime will contain the datetime to display to the user.
+     * chosenTag will be the tag name to send to the DB.
+     * status will contain a number
+     * (-1 = error, 0 = nothing, 1 = loading, 2 = success)
+     * @param props
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -16,6 +31,13 @@ class ActivateBasicTag extends Component {
         }
     }
 
+    /**
+     * At the end of the page loading, state.recentTags
+     * is filled with the 8 most recent tags.
+     * getRecentTags is imported from omgServer.js which will fetch data
+     * with '/recent' url. The url is used by routeTag with ctrTag.getRecentTagsFromUserId
+     * (ctrTag is where the orm will make the DB request)
+     */
     componentDidMount() {
         getRecentTags().then((res) => {
             if (res) {
@@ -24,8 +46,6 @@ class ActivateBasicTag extends Component {
                 console.log(res);
             }
         });
-
-        console.log('chosenDatatimeISO :\n'+new Date(this.state.chosenDatetime).toISOString());
     }
 
     // componentDidUpdate(prevProps, prevState, snapshot) {
@@ -33,28 +53,44 @@ class ActivateBasicTag extends Component {
     //         console.log(this.state.chosenTag);
     //     }
     // }
-
+    /**
+     * Called in showDatetime when the value changes.
+     * Set roundedDatetime to get the data rounded to be sent to the DB.
+     * Set chosenDatetime to display the datetime chosen
+     * (and avoid 00, 05, 50, 55 to be the only minutes we can enter)
+     * set state.status to "activate tag" (0)
+     * @param event
+     */
     chosenDatetimeChange = (event) => {
-        console.log(event.target.value);
         this.setState({roundedDatetime: this.getDatePickerFormat(new Date(event.target.value))});
         this.setState({chosenDatetime: this.getDatePickerBeforeRound(new Date(event.target.value))});
         if (this.state.status !== 0) {
             this.setState({status: 0});
         }
     }
-
+    /**
+     * Called in showRecentTags on a tag click.
+     * It set the manual input value to the chosen tag name (and display it).
+     * It set the state.chosenTag (data to be post with the datetime) and
+     * set state.status to "activate tag" (0)
+     * @param event
+     */
     recentTagClick = (event) => {
         let manualTagInput = document.getElementById("manualBasicTagInput");
         manualTagInput.value = event.target.value;
-        // if (manualTagInput.value !== "") {
-        //     manualTagInput.value = "";
-        // }
         this.setState({chosenTag: event.target.value});
         if (this.state.status !== 0) {
             this.setState({status: 0});
         }
     }
-
+    /**
+     * Called when manualTag input value changes
+     * (only by manual input not by clicking on a tag name).
+     * It gets the checked radio button (if there is) and removes the class.
+     * It sets state.chosenTag with the user input and
+     * set state.status to "activate tag" (0)
+     * @param event
+     */
     manualBasicTagInputChange = (event) => {
         let radio = document.querySelector('input[type="radio"][name="recentTagsButtons"]:checked');
         if (radio) {
@@ -67,7 +103,17 @@ class ActivateBasicTag extends Component {
             this.setState({status: 0});
         }
     }
-
+    /**
+     * Called in showBasicConfirmButton.
+     * If the manualTag input is not empty,
+     * it'll set the status to "loading",
+     * then convert the rounded datetime to
+     * a readable format for the DB.
+     * postBasicTag (omgServer.js) with url '/one',
+     * (body : endDatetime useless ?),
+     * routeTag post ctrTag.postOne
+     * Then set state.status to "success" (2) or "error" (-1)
+     */
     basicConfirmButtonClick = () => {
         if (this.state.chosenTag) {
             this.setState({status: 1});
@@ -85,8 +131,11 @@ class ActivateBasicTag extends Component {
     }
 
     /**
-     * manages the display of the status of the tag activation (basic confirm button)
-     *
+     * manages the display of the status of the tag activation (basic confirm button).
+     * Called when state.status changes, it will update the element above
+     * the button (showBasicConfirmButton) or the button text (changeBasicConfirmButtonStatus).
+     * state.status can be set in chosenDatetimeChange (0), recentTagClick (0),
+     * manualBasicTagInputChange (0) and basicConfirmButtonClick (1 then 2 or -1)
      * @return {JSX.Element}
      */
     activationResults() {
@@ -109,8 +158,12 @@ class ActivateBasicTag extends Component {
     }
 
     /**
-     * Visual change of basic confirm button
-     *
+     * Visual change of basic confirm button.
+     * Called only in activationResults.
+     * It takes the HTML element, removes the class if..
+     * then set the button attribute to disabled,
+     * the button class with the newBtnColor param
+     * and the button text with the btnText param
      * @param newBtnColor
      * @param btnText
      */
@@ -128,8 +181,13 @@ class ActivateBasicTag extends Component {
     }
 
     /**
-     * Displays recent tags
-     *
+     * Displays recent tags.
+     * Each tag name will be displayed in a radio input
+     * (only one can be selected, name is the same for each input
+     * in order to be treated as a group (deselect when one is selected)).
+     * Label is important for accessibility.
+     * recentTagClick will set the input value
+     * and set state.chosenTag to the name of the tag.
      * @return {JSX.Element}
      */
     showRecentTags() {
@@ -163,6 +221,12 @@ class ActivateBasicTag extends Component {
         }
     };
 
+    /**
+     * This input needs a string, entered manually or by clicking
+     * on the recent tags showed above.
+     * When the value changes, state.chosenTag is set with the string
+     * @returns {JSX.Element}
+     */
     manualTag() {
         return (
             <div className={"mt-3"}>
@@ -177,6 +241,17 @@ class ActivateBasicTag extends Component {
         );
     }
 
+    /**
+     * The initial value is now (datetime).
+     * onChange is called when the value changes (with a manual input or the calendar)
+     * and sets the value to the chosen datetime.
+     * The TextField is type "datetime-local" which is supported by most browser
+     * but not well implemented (firefox and safari). That leads to be forced to
+     * enter a time manually because the time picker is not showing.
+     * https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input/datetime-local#g%C3%A9rer_la_prise_en_charge_des_navigateurs
+     * Knowing that, the data displayed and sent are set separately to avoid inconsistency.
+     * @returns {JSX.Element}
+     */
     showDatetime() {
         return (
             <div className={"mt-3 d-flex flex-column"}>
@@ -192,32 +267,51 @@ class ActivateBasicTag extends Component {
         );
     }
 
+    /**
+     * It returns the date rounded the nearest multiple of 5.
+     * Only used in getDatePickerFormat
+     * @param date
+     * @returns {Date}
+     */
     roundTo5Minutes(date) {
-        // console.log('avant arrondi:\n'+date);
         let coeff = 1000 * 60 * 5;
         return new Date(Math.round(date.getTime() / coeff) * coeff);
     }
 
+    /**
+     * Used to set the initial value and
+     * the updated value (onchange) of state.chosenDatetime.
+     * The rest is the same as getDatePickerFormat.
+     * @param date
+     * @returns {string}
+     */
     getDatePickerBeforeRound(date) {
         let initDate = date;
         initDate.setUTCHours(initDate.getUTCHours() - initDate.getTimezoneOffset() / 60);
         return initDate.toISOString().substr(0, 16);
     }
 
-
+    /**
+     * date is either now or the chosen datetime (event.target.value).
+     * Used to set the initial value and
+     * the updated value (onchange) of state.roundedDatetime.
+     * It will round the date the nearest multiple of 5.
+     * It will add the timezone offset (GMT+1) to have the local hour.
+     * It converts it into iso format and removes the zeros at end.
+     * @param date
+     * @returns {string}
+     */
     getDatePickerFormat(date) {
         let initDate = this.roundTo5Minutes(date);
-        // let initDate = date;
-        // console.log('date arrondie:\n'+initDate);
-
         initDate.setUTCHours(initDate.getUTCHours() - initDate.getTimezoneOffset() / 60);
-        // console.log('date après UTC hours:\n'+initDate.setUTCHours(initDate.getUTCHours() - initDate.getTimezoneOffset() / 60));
-        // console.log('date ISO string:\n'+initDate.toISOString().substr(0, 16));
-
-
         return initDate.toISOString().substr(0, 16);
     }
 
+    /**
+     * Contains a div where requirement error will be displayed
+     * and a button which will trigger an api call.
+     * @returns {JSX.Element}
+     */
     showBasicConfirmButton() {
         return (
             <div className="mt-4 align-self-center d-flex flex-column">
@@ -229,6 +323,16 @@ class ActivateBasicTag extends Component {
         );
     }
 
+    /**
+     * CardBasicTitle wrap content with a "card" style and set the title.
+     * "d-flex flex-column" means display flex and put elements vertically.
+     * showRecentTags gives a set of the recent tags to fill the input below.
+     * manualTag is an input waiting for either a recent tag or a new one (string).
+     * showDatetime is also an input waiting for a datetime (date should be picked with the calendar).
+     * showBasicConfirmButton button which will trigger the post call with tag name and datetime.
+     * activationResults is a space where the result of the button click will be displayed.
+     * @returns {JSX.Element}
+     */
     render() {
         return (
             <CardBasicTitle title={"Activate basic tag"}>
