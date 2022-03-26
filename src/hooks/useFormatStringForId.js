3ba09942b766d2fd7682dbc17b6689a3c987f5b3
Ambroise Mostin <ambroise.mostin@amoobi.com>
2022-03-26 18:27:57 +0100

    ci fix

--- src/hooks/useFormatStringForId.js
+++ src/hooks/useFormatStringForId.js
@@ -2,6 +2,6 @@
 
 export const useFormatStringForId = (str) => {
     let regex = /[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\uFFFF]/gu;
-    let newStr = str.replaceAll(regex, '');
+    let newStr = str.replace(regex, '');
     return newStr;
 }
