4e72d10e2bf5ff05ae2033279f615219f0b52b3e
Ambroise Mostin <ambroise.mostin@amoobi.com>
2022-04-26 23:00:53 +0200

    new page: pending tags

---
+++ src/pages/PendingTags.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class PendingTags extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <div >
+                    yo yo yoooow !
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default PendingTags;
