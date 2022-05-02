00f56d132ff59f9aba72a224adb23283fdf38c9f
Ambroise Mostin <ambroise.mostin@amoobi.com>
2022-05-02 19:48:53 +0200

    split code

--- src/hooks/useHasMore.js
+++ src/hooks/useHasMore.js
@@ -1,8 +1,5 @@
-import {useState} from "react";
 
 export const useHasMore = (rcount, rhistory) => {
-    // const count = useState(rangesHistoryCount);
-    // const history = useState(rangesHistory);
     let ret = true;
     if (rcount) {
         if (rcount <= rhistory.length) {
